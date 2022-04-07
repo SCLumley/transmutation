7b12c54cb991e4e95a3b4c89901b086e394641e8
SCLumley <simon.c.lumley@gmail.com>
2022-04-07 16:19:34 +0100

    start of attempts to implement webapp.

--- webapp.py
+++ webapp.py
@@ -1,4 +1,11 @@
-import game
+import flask
+app = flask.Flask(__name__)
 
 
+@app.route('/')
+def home():
+   return flask.render_template('test.html')
+
+if __name__ == '__main__':
+    app.run()
 
