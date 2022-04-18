87e75b784950e783dcb301b79771768ce1afd0fc
Simon Lumley <simon.c.lumley@gmail.com>
2022-04-18 20:47:48 +0100

    fixed bug with domain counting

--- webapp.py
+++ webapp.py
@@ -1,10 +1,13 @@
 import flask
+from flask import send_from_directory
+import game
 app = flask.Flask(__name__)
 
 
 @app.route('/')
 def home():
-   return flask.render_template('test.html')
+   return flask.render_template('Transmutation.html')
+
 
 if __name__ == '__main__':
     app.run()
